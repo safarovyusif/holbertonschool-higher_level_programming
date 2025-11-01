f34bf2764207138f18759f801bb7131e7e531d24
safarovyusif <safarov.yusif@yahoo.com>
2025-11-01 14:49:47 +0000

    task

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+0-square_matrix_simple.py
+Function that computes the square value of all integers of a matrix.
+"""
+
+def square_matrix_simple(matrix=[]):
+    """
+    matrix: list of lists of integers
+    Returns a new matrix where each value is the square of the input value.
+    The original matrix is not modified.
+    """
+    return [[item * item for item in row] for row in matrix]
