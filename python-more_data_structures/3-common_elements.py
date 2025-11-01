777480a3c9952da10df50e181749d55b9890390b
safarovyusif <safarov.yusif@yahoo.com>
2025-11-01 15:38:31 +0000

    task

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+3-common_elements.py
+Return a set of common elements in two sets.
+"""
+
+
+def common_elements(set_1, set_2):
+    """
+    Return a new set with elements common to set_1 and set_2.
+    """
+    return set_1 & set_2
