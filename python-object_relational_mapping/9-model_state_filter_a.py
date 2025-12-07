2bf93b46047c1bf6c438208e811f4229de9214ea
safarovyusif <safarov.yusif@yahoo.com>
2025-12-07 15:23:15 +0000

    task

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa.
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    # Engine yaradılır
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    # Session yaradılır
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Sorğu: Adında 'a' hərfi olanları axtarırıq (LIKE '%a%')
+    # və id-yə görə sıralayırıq
+    states = session.query(State).filter(State.name.like('%a%')) \
+                    .order_by(State.id).all()
+
+    # Nəticələri çap edirik
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
