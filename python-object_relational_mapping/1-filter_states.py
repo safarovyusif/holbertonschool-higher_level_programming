619df5aaec5bc2c66e97041d828e87ecfefa6d4b
safarovyusif <safarov.yusif@yahoo.com>
2025-12-07 14:42:39 +0000

    task

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -6,26 +6,16 @@ from the database hbtn_0e_0_usa.
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    # Bazaya qoşuluruq
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-
-    # Sorğu: Adı 'N' ilə başlayanları (BINARY - böyük hərfi dəqiqləşdirmək üçün) seçirik
-    # Və id-yə görə sıralayırıq
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
-
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' "
+                "ORDER BY id ASC")
     rows = cur.fetchall()
-
     for row in rows:
         print(row)
-
     cur.close()
     db.close()
